165754bf7d9ef992eda4b7a685c90059dca2107a
hoeferg <111393796+hoeferg@users.noreply.github.com>
2022-09-19 21:50:05 -0700

    try to add screen end. Line broken at 65

--- assets/script.js
+++ assets/script.js
@@ -10,32 +10,32 @@ let timerInterval;
 
 let questions = [
     {
-        title: "Commonly used data types DO NOT include:",
+        topic: "Commonly used data types DO NOT include:",
         choices: ["strings", "booleans", "alerts", "numbers"],
         answer: "alerts"
     },
     {
-        title: "The condition in an if / else statements is enclosed within ________.",
+        topic: "The condition in an if / else statements is enclosed within ________.",
         choices: ["quotes", "curly brackets", "parentheses", "square bracket"],
         answer: "parentheses"
     },
     {
-        title: "The condition in an if / else statement is enclosed within__________.",
+        topic: "The condition in an if / else statement is enclosed within__________.",
         choices: ["numbers and strings", "other arrays", "booleans", "all the above"],
         answer: "other arrays"
     },
     {
-        title: "Arrays in Javascript can be used to store ____.",
+        topic: "Arrays in Javascript can be used to store ____.",
         choices: ["numbers and strings", "other arrays", "booleans", "all the above"],
         answer: "all the above"
     },
     {
-        title: "String values must be enclosed within _____ when being assigned to variables.",
+        topic: "String values must be enclosed within _____ when being assigned to variables.",
         choices: ["commas", "curly brackets", "quotes", "parenthesis"],
         answer: "quotes"
     },
     {
-        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        topic: "A very useful tool for used during development and debugging for printing content to the debugger is:",
         choices: ["Javascript", "terminal /bash", "for loops", "console.log"],
         answer: "console.log"
     },
@@ -62,7 +62,7 @@ function setTime() {
 
 function render() {
     questionDiv.innerHTML = "";
-    questionDiv.textContent = questions[questionCard].title
+    questionDiv.textContent = questions[questionCard].topic
     for (let i = 0; i < questions[questionCard].choices.length; i++) {
         let userChoices = questions[questionCard].choices[i]
         let choiceButton = document.createElement("button")
@@ -76,8 +76,9 @@ function render() {
 
 // function sendMessage
 
-function quizEnd () {
+function quizEnd() {
     clearInterval(timerInterval);
+
 
 }
 
@@ -97,8 +98,23 @@ function submitAnswer() {
             quizEnd()
         }
     }
-
 }
+
+function score() {
+    let score = secondsLeft
+    alert("Your score is" + score + "!")
+} 
+
+
+generateBtn.addEventListener("click", generateQuiz);
+// localStorage.setItem("highScore", 0);
+
+// if (score > parseInt(localStorage.getItem("hightScore"))) {
+//     localStorage.setItem("hightScore", score);
+// }
+
+
+
 // need start button to interact when clicked. It needs to start a timer and presents with a timer
 
 // A way to tell when the answer is right or wrong
@@ -110,4 +126,4 @@ function submitAnswer() {
 // When all the questions are answered or timer reaches 0 then the game is over
 
 // Then you need to put your initials and save score
-generateBtn.addEventListener("click", generateQuiz);
+
