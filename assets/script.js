361b5fc3d5ff1166a25fbb4e699cc1b96fbda4a8
hoeferg <111393796+hoeferg@users.noreply.github.com>
2022-09-19 17:25:41 -0700

    added question that rotate through and added end of clock.

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,16 @@
 const generateBtn = document.querySelector("#generate-quiz");
 const startCard = document.querySelector("#start-quiz");
-const questionCard = document.querySelector("#question");
 const $time = document.querySelector(".time");
 const $submitAnswer = document.querySelector("#next");
+const questionDiv = document.querySelector("#question");
+let questionCard = 0
+let secondsLeft = 101
+let timerInterval;
+
 
 let questions = [
     {
-        title: "Commonly used data types DO NOt include:",
+        title: "Commonly used data types DO NOT include:",
         choices: ["strings", "booleans", "alerts", "numbers"],
         answer: "alerts"
     },
@@ -18,7 +22,7 @@ let questions = [
     {
         title: "The condition in an if / else statement is enclosed within__________.",
         choices: ["numbers and strings", "other arrays", "booleans", "all the above"],
-        answer: "parentheses"
+        answer: "other arrays"
     },
     {
         title: "Arrays in Javascript can be used to store ____.",
@@ -32,41 +36,68 @@ let questions = [
     },
     {
         title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
-        choices: ["Javascript", "terminal /bash", "for loops", "console log"],
-        answer: "console log"
+        choices: ["Javascript", "terminal /bash", "for loops", "console.log"],
+        answer: "console.log"
     },
 ]
 
 function generateQuiz() {
-    startCard.setAttribute("style","display:none")
-    $time.setAttribute("style","display:block")
+    startCard.setAttribute("style", "display:none")
+    $time.setAttribute("style", "display:block")
     setTime()
+    render()
 }
 
+
 function setTime() {
-    let secondsLeft = 101
-    const timerInterval = setInterval (function() {
+    timerInterval = setInterval(function () {
         secondsLeft--;
-        $time.textContent = "Time: " + secondsLeft + " seconds"; 
-        if(secondsLeft === 0) {
+        $time.textContent = "Time: " + secondsLeft + " seconds";
+        if (secondsLeft === 0) {
             clearInterval(timerInterval);
             return;
         }
-    },1000);
+    }, 1000);
+}
+
+function render() {
+    questionDiv.innerHTML = "";
+    questionDiv.textContent = questions[questionCard].title
+    for (let i = 0; i < questions[questionCard].choices.length; i++) {
+        let userChoices = questions[questionCard].choices[i]
+        let choiceButton = document.createElement("button")
+        choiceButton.textContent = userChoices
+        choiceButton.setAttribute("data-value", userChoices)
+        choiceButton.addEventListener("click", submitAnswer)
+        questionDiv.appendChild(choiceButton)
+    }
 }
 
 
 // function sendMessage
 
+function quizEnd () {
+    clearInterval(timerInterval);
 
-let submitAnswer = function() {
-    let answer = document.getElementsByName('#question');
-    let length =questions.length;
-    for (var i = 0; i <length; i++); {
-        if (answer[i].checked) {
-            val = answer[i].value;
+}
+
+
+function submitAnswer() {
+    console.log(this.dataset.value)
+    if (this.dataset.value === questions[questionCard].answer) {
+        questionCard++
+        if (secondsLeft === 0 || questionCard === questions.length - 1) {
+            quizEnd()
+        }
+        render()
+    }
+    else {
+        secondsLeft = secondsLeft - 5
+        if (secondsLeft === 0) {
+            quizEnd()
         }
     }
+
 }
 // need start button to interact when clicked. It needs to start a timer and presents with a timer
 
